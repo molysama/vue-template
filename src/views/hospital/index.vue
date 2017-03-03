a613e03dfb061f9fc163dba9157169d75ac3c2bf
lgqlee <xxgr02@163.com>
2017-03-03 10:37:09 +0800

    add iconfont

--- src/views/hospital/index.vue
+++ src/views/hospital/index.vue
@@ -1,10 +1,23 @@
 
 <template lang="jade">
 
+.body 
+  header
+    .change
+    .search
+
+
 </template>
 <script>
   
 </script>
 <style lang="stylus" scoped>
+
+header 
+  width 100%
+  height 1.28rem 
+  line-height @height
+  background #f9f9f9
+  margin 0 auto
   
 </style>
